41370f02821fdefc9b7ba9ca7da8fd781f3dd6ef
wolfiee42 <saifalislam2022@gmail.com>
2025-07-22 20:25:15 +0600

    fix: resolving api and lint issues.

--- src/middleware/notFoundAPI.ts
+++ src/middleware/notFoundAPI.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import { StatusCodes } from 'http-status-codes'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const notFoundAPI = (req: Request, res: Response, next: NextFunction) => {
   res.status(StatusCodes.NOT_FOUND).json({
     success: false,
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,32 +1,26 @@
-import AppError from '../errors/appError';
-import { UserModel } from '../modules/user/user.model';
+import AppError from '../errors/appError'
+import { UserModel } from '../modules/user/user.model'
 
 export const checkUser = async (
   userId: string,
 ): Promise<ErrorConstructor | boolean> => {
-  const user = await UserModel.findById(userId);
+  const user = await UserModel.findById(userId)
   if (!user) {
-    throw new AppError(404, 'User not found');
+    throw new AppError(404, 'User not found')
   }
   if (user.isBanned) {
     throw new AppError(
       403,
-      `${user.name} has no permission to access this resource`,
-    );
+      `${user.username} has no permission to access this resource`,
+    )
   }
-  if (user.isSetInfo) {
-    throw new AppError(
-      403,
-      `${user.name} Please set your Account information to Activate your account`,
-    );
-  }
-  return true;
-};
+  return true
+}
 
 export const isUsernameAvailable = async (username: string) => {
-  const user = await UserModel.findOne({ username });
+  const user = await UserModel.findOne({ username })
   if (user) {
-    throw new AppError(409, `Username ${username} is already taken`);
+    throw new AppError(409, `Username ${username} is already taken`)
   }
-  return true;
-};
+  return true
+}
